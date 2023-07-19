c2aa53e3cf219be87111f0582a975149765098ac
liutristan <113070063+liutristan@users.noreply.github.com>
2023-07-19 13:10:13 -0400

    removed ads, fixed iframe & shuttleai, cloaking
    removed ads from games and shuttle ai to fix the iframe, cleaned up the looks of the page, fixed shuttleai by reverting uv update, and added some cloaking renaming games and shuttle ai to math and classes
    
    Signed-off-by: liutristan <113070063+liutristan@users.noreply.github.com>

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,11 +1,9 @@
-/*global Ultraviolet*/
 self.__uv$config = {
-    prefix: '/ultraviolet/',
+    prefix: '/shuttle/',
     bare: '/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
-    client: '/uv/uv.client.js',
     bundle: '/uv/uv.bundle.js',
     config: '/uv/uv.config.js',
     sw: '/uv/uv.sw.js',
