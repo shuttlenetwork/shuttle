4937c8903ab17974b5fb1065bc273ff4c01fe81e
tristandevs <113070063+tristandevs@users.noreply.github.com>
2023-10-27 08:51:42 -0500

    v3 🚀
    v3

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,39 +1,46 @@
 function fullscreen() {
-	var elem = document.getElementById("ifr")
-	if (elem.requestFullscreen) {
-		elem.requestFullscreen();
-	} else if (elem.webkitRequestFullscreen) { /* Safari */
-		elem.webkitRequestFullscreen();
-	} else if (elem.msRequestFullscreen) { /* IE11 */
-		elem.msRequestFullscreen();
-	}
+  var elem = document.getElementById("ifr")
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
 }
 
+
 async function registerSW() {
-	if (!("serviceWorker" in navigator)) return;
-	const workerURL = "/uv.sw-handler.js";
-	const worker = await navigator.serviceWorker.getRegistration(workerURL);
-	if(worker) return worker;
-	return navigator.serviceWorker.register(workerURL, { scope: __uv$config.prefix });
+  await navigator.serviceWorker.register("/dynamic.sw-handler.js", {
+    scope: "/shuttle-dn",
+  });
+  const workerURL = "/uv.sw-handler.js";
+  const worker = await navigator.serviceWorker.getRegistration(workerURL, {
+    scope: "/shuttle-uv",
+  });
+  if (worker) return worker;
+  return navigator.serviceWorker.register(workerURL, { scope: __uv$config.prefix });
 }
 
 registerSW();
 
 function setFavicon(f) {
-	var link = document.querySelector("link[rel~='icon']");
-	if (!link) {
-		link = document.createElement("link");
-		link.rel = "icon";
-		document.head.appendChild(link);
-	}
-	link.href = f;
+  var link = document.querySelector("link[rel~='icon']");
+  if (!link) {
+    link = document.createElement("link");
+    link.rel = "icon";
+    document.head.appendChild(link);
+  }
+  link.href = f;
 }
 
 function encodeUVUrlWithPath(url = "") {
-	return __uv$config.prefix + __uv$config.encodeUrl(url);
+  return __uv$config.prefix + __uv$config.encodeUrl(url);
 }
 
 window.addEventListener("load", () => {
-	if (localStorage.getItem("title")) document.title = localStorage.getItem("title");
-	if (localStorage.getItem("favicon")) setFavicon(localStorage.getItem("favicon"));
+  if (localStorage.getItem("title")) document.title = localStorage.getItem("title");
+  if (localStorage.getItem("favicon")) setFavicon(localStorage.getItem("favicon"));
 });
+
+
--- public/assets/js/settings.js
+++ public/assets/js/settings.js
@@ -13,6 +13,18 @@ function changeSearch(target) {
       localStorage.setItem("search", "Google")
   }
 }
+function changeProxy(target) {
+  switch (target.value) {
+    case "Dynamic":
+      localStorage.setItem("proxy", "Dynamic");
+      break;
+    case "Ultraviolet":
+      localStorage.setItem("proxy", "Ultraviolet");
+      break;
+    default: 
+      localStorage.setItem("proxy", "Dynamic")
+  }
+}
 
 function changeFavicon(value) {
 	setFavicon(value);
@@ -28,3 +40,28 @@ function abc() {
 	window.open().document.body.innerHTML =`<iframe style="height:100%; width: 100%; border: none; position: fixed; top: 0; right: 0; left: 0; bottom: 0; border: none" sandbox="allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-popups allow-popups-to-escape-sandbox allow-presentation allow-same-origin allow-scripts allow-top-navigation allow-top-navigation-by-user-activation" src="${window.location.href}"></iframe>`;
 	window.location = "https://google.com";
 }
+
+function changeTheme(value) {
+  localStorage.setItem("themehex", value);
+  document.body.style.backgroundColor = value;
+  document.querySelector('#colorPicker').value = value;
+}
+
+function resetTheme() {
+  localStorage.removeItem("themehex");
+  document.body.style.backgroundColor = "#0b0b0b";
+  document.querySelector('#colorPicker').value = "#0b0b0b";
+}
+
+function initTheme() {
+  const savedTheme = localStorage.getItem("themehex");
+  if (savedTheme) {
+    document.body.style.backgroundColor = savedTheme;
+    document.querySelector('#colorPicker').value = savedTheme;
+  }
+}
+
+window.addEventListener('load', initTheme);
+
+
+
